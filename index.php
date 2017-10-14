b47c31c296edd862f33eefd347ea8813cee1695e
Kevin Atkins <kaitoxas@gmail.com>
2017-10-14 13:12:56 -0600

    added <header>, <h1>, <h2>, <h3>, <ul>, and <li> tags changed title of page to 'Data Design Project' changed header of page to 'Data Design - Conceptual Design' added 'Entities & Attributes': 'Post' & 'Comment'

--- index.php
+++ index.php
@@ -2,13 +2,22 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Data Design</title>
+		<title>Data Design Project</title>
 	</head>
 	<body>
 		<header>
-			<h1>Data Design</h1>
+			<h1>Data Design - Conceptual Design</h1>
 		</header>
 		<main>
+			<h2>Entities & Attributes</h2>
+			<h3>Post</h3>
+			<ul>
+				<li></li>
+			</ul>
+			<h3>Comment</h3>
+			<ul>
+				<li></li>
+			</ul>
 		</main>
 	</body>
 </html>
