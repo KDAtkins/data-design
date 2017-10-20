a0c96165e52667e1969b78aba3ae5e8ba145634b
Kevin Atkins <kaitoxas@gmail.com>
2017-10-20 09:38:24 -0600

    created 'reddit.sql' file

---
+++ sql/reddit.sql
@@ -0,0 +1,19 @@
+-- Select for Entities
+SELECT * FROM profile;
+SELECT  * FROM post;
+SELECT * FROM comment;
+
+-- Update for Entities
+UPDATE profile SET profilePhone = "12345678899";
+UPDATE post SET postContent = "Hello World";
+UPDATE comment SET commentContent = "MySQL is a lot of work";
+
+-- Delete for Entities
+DELETE FROM profile WHERE profileEmail;
+DELETE FROM post WHERE postTopic;
+DELETE FROM comment WHERE commentContent;
+
+-- Insert for Entities
+INSERT INTO profile VALUES ();
+INSERT INTO post VALUES ();
+INSERT INTO comment VALUES ();
