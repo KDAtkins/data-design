238efa99cfef033b73fd565a8a91c63e65703847
Kevin Atkins <kaitoxas@gmail.com>
2017-10-24 09:00:28 -0600

    installed composer.json

--- classes/profile.php
+++ classes/profile.php
@@ -7,7 +7,7 @@
  **/
 namespace Edu\Cnm\DataDesign;
 
-require_once ("auto_load.php");
+require_once ("autoload.php");
 require_once (dirname(__DIR__, 2) . "/vendor/autoload.php");
 
 use Ramsey\Uuid\Uuid;
