e6671523ddd58ebb7cd3f9971205ccc6f995cb86
Kevin Atkins <kaitoxas@gmail.com>
2017-10-24 11:09:14 -0600

    included all accessor and mutator methods for "Profile" class, may need additional modifications to meet project requirements

--- classes/profile.php
+++ classes/profile.php
@@ -13,6 +13,8 @@ require_once (dirname(__DIR__, 2) . "/vendor/autoload.php");
 use Ramsey\Uuid\Uuid;
 
 class Profile implements \JsonSerializable {
+	use ValidateUuid;
+	use ValidateDate;
 	/**
 	 * id for profile id (primary key)
 	 * @var Uuid $profileID
@@ -58,7 +60,7 @@ class Profile implements \JsonSerializable {
 		try {
 			$this->setProfileId($newProfileId);
 			$this->setProfileActivationToken($newProfileActivationToken);
-			$this->setProfileHandle($newProfileAtHandle);
+			$this->setProfileAtHandle($newProfileAtHandle);
 			$this->setProfileEmail($newProfileEmail);
 			$this->setProfileHash($newProfileHash);
 			$this->setProfileSalt($newProfileSalt);
@@ -69,6 +71,123 @@ class Profile implements \JsonSerializable {
 		}
 	}
 
+	/**
+	 * accessor method for profile id
+	 *
+	 * @return Uuid value of profile id
+	 **/
+	public function getProfileId() : Uuid {
+		return ($this->profileID);
+	}
+
+	/**
+	 * mutator method for profile id
+	 *
+	 * @param Uuid/string $newProfileId new value of profile id
+	 * @throws \RangeException if $newProfileId is not positive
+	 * @throws \TypeError if $newProfileId is not a uuid or string
+	 **/
+	public function setProfileId($newProfileId) : void {
+		try {
+			$uuid = self::validateUuid($newProfileId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw (new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+
+		// convert and store the profile id
+		$this->profileID = $uuid;
+	}
+
+	/**
+	 * accesor method for profile activation token
+	 *
+	 * @return mixed value for profile activation token
+	 */
+	public function getProfileActivationToken() : string {
+		return $this->profileActivationToken;
+	}
+
+	/**
+	 * mutator method for profile activation
+	 *
+	 * @param mixed $newProfileActivation
+	 */
+	public function setProfileActivationToken($newProfileActivation) : void {
+		$this->profileActivationToken = $newProfileActivation;
+	}
+
+	/**
+	 * accessor method for profile at handle
+	 *
+	 * @return string value for profile at handle
+	 */
+	public function getProfileAtHandle() : string {
+		return $this->profileAtHandle;
+	}
+
+	/**
+	 * mutator method for profile at handle
+	 *
+	 * @param mixed $newProfileAtHandle
+	 */
+	public function setProfileAtHandle($newProfileAtHandle) : void {
+		$this->profileAtHandle = $newProfileAtHandle;
+	}
+
+	/**
+	 * accessor method for profile email
+	 *
+	 * @return string value of profile email
+	 */
+	public function getProfileEmail() : string {
+		return $this->profileEmail;
+	}
+
+	/**
+	 * mutator method for profile email
+	 *
+	 * @param mixed $newProfileEmail
+	 */
+	public function setProfileEmail($newProfileEmail) : void {
+		$this->profileEmail = $newProfileEmail;
+	}
+
+	/**
+	 * accessor method for profile hash
+	 *
+	 * @return mixed value for profile hash
+	 */
+	public function getProfileHash() : string {
+		return $this->profileHash;
+	}
+
+	/**
+	 * mutator method for profile hash
+	 *
+	 * @param mixed $newProfileHash
+	 */
+	public function setProfileHash($newProfileHash) : void {
+		$this->profileHash = $newProfileHash;
+	}
+
+	/**
+	 * accessor method for profile salt
+	 *
+	 * @return mixed value for profile salt
+	 */
+	public function getProfileSalt() : string {
+		return $this->profileSalt;
+	}
+
+	/**
+	 * mutator method for profile salt
+	 *
+	 * @param mixed $newProfileSalt
+	 */
+	public function setProfileSalt($newProfileSalt) : void {
+		$this->profileSalt = $newProfileSalt;
+	}
 
 	/**
 	 * formats the state variables for JSON serialization
@@ -76,6 +195,10 @@ class Profile implements \JsonSerializable {
 	 * @return array resulting state variables to serialize
 	 */
 	public function jsonSerialize() {
-		// TODO: Implement jsonSerialize() method.
+		$fields = get_object_vars($this);
+
+		$fields["profileId"] = $this->profileID;
+
+		return($fields);
 	}
 }
